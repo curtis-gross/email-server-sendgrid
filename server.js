bb06af140015820d2dcf2fa739588a5aa13133f9
Curtis <curtis.gross@lytics.io>
2018-01-10 15:51:47 -0800

    updated vars

--- server.js
+++ server.js
@@ -16,23 +16,12 @@ var fs = require('fs');
 console.log("begin startup");
 
 server = http.createServer( function(req, res) {
-	 console.log("entered into server setup");
-	 
-	 res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
+	console.log("entered into server setup"); 
+	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-
-    console.dir(req.param);
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    //console.dir(req.param);
 
     if (req.method == 'POST') {
         console.log("POST");
