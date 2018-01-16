45bf68e4b9c6e2625cfbde1b76aafbb6d8c514ad
Curtis <curtis.gross@lytics.io>
2018-01-16 09:09:23 -0800

    Added some explanation to code
    Added explaination to the code

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 /* 
+My current process to deploy code to Heroku (auto spins up node server)
 git add server.js 
 git commit -m "trimmed code"
 git push heroku master 
-    
-    */
+*/
 
 const sgMail = require('@sendgrid/mail');
+// This step requires setting up an environment variable with your sendgrid API key -> Heroku.  This code then looks for this environment variable to use when communicating with heroku. 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+//Required node packages (node install xyz)
 var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var fs = require('fs');
 
+// HTTP server to listen for inbound POST requests from Lytics Form
 console.log("begin startup");
-
 server = http.createServer( function(req, res) {
 	console.log("entered into server setup"); 
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,7 +32,7 @@ server = http.createServer( function(req, res) {
         req.on('end', function () {
             console.log("Body: " + body);
         });
-      
+      // Sendgrid email configuration to send over to sendgrid.
             const msg = {
 			  to: body,
 			  from: 'test-no-reply@ticly.com',
@@ -38,9 +40,10 @@ server = http.createServer( function(req, res) {
 			  text: 'Welcome to Ticly, ' + body + ' we appreciate your signup.',
 			  html: '<h3>Welcome to Ticly</h3>' + body + ' we appreciate your signup.',
 			  type: 'text/plain',
-			  value: 'heya!',
+			  value: 'Ticly Welcome Email',
 			  template_id: '2ec278d7-85c5-4329-9bf5-3fc5b27238e3'
 			};
+			// Little bit of terminal console output for debugging
 			console.log("about to send email");
 			console.log(msg);
 			console.log(body + ' successfully emailed');
@@ -48,6 +51,7 @@ server = http.createServer( function(req, res) {
 			sgMail.send(msg).catch(function () {
 				console.log("Message Rejected");
 			});
+		// if things failed or succeeded this message shows that the service is ready for the next attempt
 		console.log("Awaiting next email");
             
         });
@@ -55,8 +59,6 @@ server = http.createServer( function(req, res) {
 	}
 	
 });
+// launch the server (with all the magic above)
 server.listen(process.env.PORT)
 console.log('listening on Port ' + process.env.PORT);
-
-//sendgrid template id : 1138a7de-83f3-40de-a95e-ded29e89cddd
-//
